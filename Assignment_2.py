e8142c885a2d78732a925cf434f3499bb180e53c
mcj-bien <jabienmariacristina1221@gmail.com>
2026-01-27 21:41:18 +0800

    Add files via upload
    Assignment_2

---
+++ Assignment_2.py
@@ -0,0 +1,18 @@
+# Create variables with my personal information
+fullname = "Maria Cristina Jabien"       # String
+age = 19                                 # Integer
+height_meters = 1.62                          # Float
+currently_working = False                # Boolean
+
+# Display values and their data types
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("Age:", age)
+print(type(age))
+
+print("Height in meters:", height_meters)
+print(type(height_meters))
+
+print("Currently working?", currently_working)
+print(type(currently_working))
