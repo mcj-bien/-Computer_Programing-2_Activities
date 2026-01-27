036202d7a236b9c7b850a9fa5b39d364223ea302
mcj-bien <jabienmariacristina1221@gmail.com>
2026-01-27 20:53:04 +0800

    Add files via upload
    Assignment_1

---
+++ Assignment_1.py
@@ -0,0 +1,24 @@
+score1 = 60
+score2 = 75
+score3 = 74
+score4 = 100
+
+if score1 >= 75:
+    print("Score 60: Passed")
+else:
+    print("Score 60: Failed")
+
+if score2 >= 75:
+    print("Score 75: Passed")
+else:
+    print("Score 75: Failed")
+
+if score3 >= 75:
+    print("Score 74: Passed")
+else:
+    print("Score 74: Failed")
+
+if score4 >= 75:
+    print("Score 100: Passed")
+else:
+    print("Score 100: Failed")
